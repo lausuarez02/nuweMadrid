b44d65672d5ed866b0fb291f846e5519b928c86d
Lautaro Suarez <lautarosuarez@MacBook-Air-de-Lautaro.local>
2021-11-27 01:36:09 +0100

    fix deploy problem

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import { Routes ,Route ,BrowserRouter} from 'react-router-dom';
+import { Routes ,Route ,HashRouter} from 'react-router-dom';
 import Home from 'screens/Home/Home';
 
 function App() {
 
   return (
     <div>
-      <BrowserRouter>
+      <HashRouter>
       <Routes>
         <Route path='/' element={<Home/>}/>
       </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
